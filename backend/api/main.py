76a476336aef7fcc066f0323a97532240ee2be3a
ItIsAnyx <a.m.tichonov257@gmail.com>
2026-05-05 20:53:39 +0300

    v.0.1.2 Реализация автоматической загрузки данных из датасета в БД

--- backend/api/main.py
+++ backend/api/main.py
@@ -4,6 +4,7 @@ from config import settings, validate_key
 from dataset import get_or_generate_dataset
 from database.tables import Base
 from database.db import engine
+from database.populate import populate_db
 
 app = FastAPI(title=settings.APP_NAME,
               version=settings.APP_VERSION)
@@ -16,11 +17,11 @@ async def Start():
     init_db()
 
     df = get_or_generate_dataset("retail_forecasting_dataset.csv")
-    app.state.dataset = df # Временно, чтоб датасет было видно в памяти приложения
-    print("Датасет успешно загружен в приложение.")
     print(f"Размер датасета: {df.shape}")
-    print(df.head())
+    df["Date"] = pd.to_datetime(df["Date"])
+    df["Weekend"] = df["Weekend"].astype(bool)
 
+    populate_db(df)
 
 def init_db():
     Base.metadata.create_all(bind=engine)
